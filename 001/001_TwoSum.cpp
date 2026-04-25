0225aff5608ca84a84db8748c5f292d127c7ce58
FictionSky <Fictionsky@outlook.com>
2026-04-25 15:50:31 +0800

    001

--- 001/001_TwoSum.cpp
+++ 001/001_TwoSum.cpp
@@ -8,7 +8,6 @@ using namespace std;
 
 class Solution {
 public:
-    // 这里是你刚刚写下的核心算法
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> numMap;
         for (int i = 0; i < nums.size(); i++) {
@@ -16,7 +15,7 @@ public:
             if (numMap.count(complement)) {
                 return {numMap[complement], i};
             }
-            numMap[nums[i]] = i;
+            numMap[nums[i]] = i;  // 将数字和索引建立哈希表
         }
         return {};
     }
@@ -25,14 +24,11 @@ public:
 int main() {
     Solution sol;
     
-    // 第三步：在 main 函数中写测试用例
     vector<int> nums = {2, 7, 11, 15};
-    int target = 9;
+    int target = 13;
     
-    // 调用你的算法
     vector<int> result = sol.twoSum(nums, target);
     
-    // 打印输出结果来验证
     if (result.size() == 2) {
         cout << "Output: [" << result[0] << ", " << result[1] << "]" << endl;
     } else {
